abafc9557fce79a609597026c6fa7129288ac4f9
Naie Htoo Aung <k025g1013@.g.neec.ac.jp>
2026-04-13 16:21:02 +0900

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <Windows.h>
+#include <KamataEngine.h>
+#include "GameScene.h"
+// Windowsアプリでのエントリーポイント(main関数)
+using namespace KamataEngine;
+int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+	KamataEngine::Initialize(L"GC2A_04_ネイ_トウーアウン_AL3");
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+	GameScene* gameScene = new GameScene();
+	gameScene->Initialize();
+	// ゲームループ
+	while (true) {
+
+		if (KamataEngine::Update()) {
+			break;
+		}
+
+		gameScene->Update();
+		dxCommon->PreDraw();
+		// 描画処理
+
+		gameScene->Draw();
+
+		dxCommon->PostDraw();
+	}
+	delete gameScene;
+	gameScene = nullptr;
+
+	KamataEngine::Finalize();
+	return 0;
+}
