ee2f9dadd34e9202926ad1b003083fd8e7c78a7e
Naie Htoo Aung <k025g1013@.g.neec.ac.jp>
2026-04-20 16:27:47 +0900

    final touch

--- GameScene.cpp
+++ GameScene.cpp
@@ -4,51 +4,53 @@ using namespace KamataEngine;
 
 
 void GameScene::Initialize() {
-	textureHandle_ = TextureManager::Load("./Resources/uvChecker.png"); 
-	sprite_ = Sprite::Create(textureHandle_, {100, 50});
-	textureHandle1_ = TextureManager::Load("./Resources/mario.jpg");
-	soundDataHandle_ = Audio::GetInstance()->LoadWave("./Resources/fanfare.wav");
-	Audio::GetInstance()->PlayWave(soundDataHandle_);
-	voiceHandle_ = Audio::GetInstance()->PlayWave(soundDataHandle_, true);
+	//textureHandle_ = TextureManager::Load("./Resources/mario.png"); 
+	//sprite_ = Sprite::Create(textureHandle_, {100, 50});
+	textureHandle1_ = TextureManager::Load("./Resources/mario.png");
 	model_ = Model::Create();
+	//soundDataHandle_ = Audio::GetInstance()->LoadWave("./Resources/fanfare.wav");
+	//Audio::GetInstance()->PlayWave(soundDataHandle_);
+	//voiceHandle_ = Audio::GetInstance()->PlayWave(soundDataHandle_, true);
 	worldTransform_.Initialize();
 	camera_.Initialize();
+	debugCamera_ = new DebugCamera(1280, 720);
+	AxisIndicator::GetInstance()->SetVisible(true);
+	AxisIndicator::GetInstance()->SetTargetCamera(&debugCamera_->GetCamera());
 }
 
 void GameScene::Update() {
 
-	Vector2 position = sprite_->GetPosition();
+	//Vector2 position = sprite_->GetPosition();
+	//sprite_->SetPosition(position);
+	debugCamera_->Update();
+	//if (Input::GetInstance()->TriggerKey(DIK_SPACE)) {
 
-	position.x += 2.0f;
-	position.y += 1.0f;
+	//	Audio::GetInstance()->StopWave(voiceHandle_);
 
-	sprite_->SetPosition(position);
-	if (Input::GetInstance()->TriggerKey(DIK_SPACE)) {
-
-		Audio::GetInstance()->StopWave(voiceHandle_);
-
-	}
-	ImGui::Begin("Debug1");
-	ImGui::Text("Kamata Tarou ", 2050, 12, 31);
-	ImGui::InputFloat3("InputFloat3", inputFloat3);
-	ImGui::SliderFloat3("SliderFloat3", inputFloat3, 0.0f, 1.0f);
-	ImGui::End();
+	//}
+	//debugCamera_->Update();
+	//ImGui::Begin("Debug1");
+	//ImGui::Text("Kamata Tarou ", 2050, 12, 31);
+	//ImGui::InputFloat3("InputFloat3", inputFloat3);
+	//ImGui::SliderFloat3("SliderFloat3", inputFloat3, 0.0f, 1.0f);
+	//ImGui::End();
 }
 
 void GameScene::Draw() {
 
-	Sprite::PreDraw();
+	//Sprite::PreDraw();
 
-	sprite_->Draw();
+	//sprite_->Draw();
 
-	Sprite::PostDraw();
+	//Sprite::PostDraw();
 
 	Model::PreDraw();
-	model_->Draw(worldTransform_, camera_, textureHandle1_);
+	model_->Draw(worldTransform_, debugCamera_->GetCamera(), textureHandle1_);
 	Model::PostDraw();
 }
 GameScene::~GameScene() {
-	delete sprite_;   
+	//delete sprite_;   
 	delete model_;
+	delete debugCamera_;
 }
 
