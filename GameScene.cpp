fec2879aba23d0ce2eba90ee945c6d4698d0bb17
Naie Htoo Aung <k025g1013@.g.neec.ac.jp>
2026-04-20 15:03:27 +0900

    update model

--- GameScene.cpp
+++ GameScene.cpp
@@ -4,6 +4,32 @@ using namespace KamataEngine;
 
 void GameScene::Initialize() {}
 
-void GameScene::Update() {}
+void GameScene::Initialize() {
+	
+	textureHandle1_ = TextureManager::Load("./Resources/mario.png");
+	model_ = Model::Create();
+	worldTransform_.Initialize();
+	camera_.Initialize();
+}
 
-void GameScene::Draw() {}
+void GameScene::Update() {
+
+
+	
+}
+
+void GameScene::Draw() {
+
+	Model::PreDraw();
+
+	model_->Draw(worldTransform_,camera_,textureHandle1_);
+
+	Model::PostDraw();
+	
+
+}
+GameScene::~GameScene() {
+	  
+	delete model_;
+}
+
--- GameScene.h
+++ GameScene.h
@@ -9,4 +9,12 @@ public:
 	void Update();
 
 	void Draw();
+	~GameScene();
+
+private:
+	
+	uint32_t textureHandle1_ = 0;
+	KamataEngine::WorldTransform worldTransform_;
+	KamataEngine::Camera camera_;
+	KamataEngine::Model* model_ = nullptr;
 };
